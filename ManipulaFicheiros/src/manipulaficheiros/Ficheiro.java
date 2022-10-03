b4a3405ba41ff9259febd91a47b9a24668a00ea3
Thatiane Leite <thatianelzago@gmail.com>
2022-10-03 15:07:41 +0100

    mostra lista de ficheiros

--- ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
+++ ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package manipulaficheiros;
 
-/**
- *
- * @author tleite
- */
+import java.io.File;
+
+
 class Ficheiro {
+
+    static void criarFicheiro() {
+    }
+
+    static void verificaSeExiste() {
+    }
+
+    static void escreveNoFinalFicheiro() {
+     }
+
+    static void escreveFicheiroNOvo() {
+    }
+
+    static void mostraConteudoFicheiro() {
+     }
+
+    static void mostraListaFicheiros() {
+        File ficheiro = new File("C:\\Users\\TLeite\\Desktop\\FicheiroJAVA\\ficheirosJAVA");
+        File [] lista = ficheiro.listFiles();
+        System.out.println("»»»» Lista de Ficheiros ««««");
+        for(int x=0;x<lista.length;x++){
+            System.out.println(lista[x].getName());
+        }
+    }
     
 }
