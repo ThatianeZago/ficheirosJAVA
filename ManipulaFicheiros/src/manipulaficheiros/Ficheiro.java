369caff2e05ec6b80cf204358a2cca25e1975fa4
Thatiane Leite <thatianelzago@gmail.com>
2022-10-03 16:47:51 +0100

    mostrar conteúdo do ficheiro

--- ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
+++ ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
@@ -1,12 +1,13 @@
 
 package manipulaficheiros;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 class Ficheiro {
@@ -65,15 +66,62 @@ class Ficheiro {
               }
        }
        
-       
-       
+     
+ 
     
-   static void escreveFicheiroNOvo() {
+    static void escreveFicheiroNOvo() {
+       String nome;
+       System.out.println("Insira o nome do ficheiro: ");
+       nome = ManipulaFicheiros.ler.nextLine();
+       nome = ManipulaFicheiros.ler.next();
+       File ficheiro = new File(nome);
+       
+           try {
+               if(!ficheiro.exists()){
+                   ficheiro.createNewFile();
+               }
+                FileWriter fw = new FileWriter(ficheiro);  ////TRUE
+                BufferedWriter bw = new BufferedWriter (fw); 
+                String texto; 
+                System.out.println("Insira o conteudo"); 
+                texto = ManipulaFicheiros.ler.nextLine(); 
+                texto = ManipulaFicheiros.ler.nextLine(); 
+                bw.write(texto); 
+                bw.newLine(); 
+                bw.close(); 
+                fw.close();  
+           } catch (IOException ex) {
+               ex.getStackTrace();
+              }
     }
 
     static void mostraConteudoFicheiro() {
-     }
-
+        String nome;
+        System.out.println("Insira o nome do ficheiro a verificar :");
+        nome = ManipulaFicheiros.ler.nextLine();
+        nome = ManipulaFicheiros.ler.next();
+        File ficheiro = new File (nome);
+        if(!ficheiro.exists()){
+            System.out.println("Oficheiro não existe");
+        }else{
+            
+            try{
+                FileReader fr = new FileReader (ficheiro);
+                BufferedReader br = new BufferedReader (fr);
+                while (br.ready()){
+                    String linha = br.readLine ();
+                    System.out.println (linha);
+                }
+                br.close();
+                br.close();
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (IOException ioe) {
+                
+            }
+        }
+    }      
+                     
     static void mostraListaFicheiros() {
         File ficheiro = new File("C:\\Users\\TLeite\\Desktop\\FicheiroJAVA\\ficheirosJAVA");
         File [] lista = ficheiro.listFiles();
